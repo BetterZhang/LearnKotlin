554ca49bc59dfa734b7d0eaa03900e82c38cbd46
zhangzhongqiang <zhangzhongqiang@jsdttec.com>
2017-06-16 10:48:32 +0800

    Write MainActivity by Kotlin

--- app/src/main/java/com/betterzhang/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/betterzhang/learnkotlin/MainActivity.kt
@@ -2,11 +2,41 @@ package com.betterzhang.learnkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        tv_content.text = "Hello Kotlin"
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "fab on click...", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show()
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_settings) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
